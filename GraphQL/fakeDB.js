242c57e21e8662fafdad3b2dd6eb94d1e3d73d28
CQ-AS <aparna.sharma@codequotient.com>
2026-05-22 14:02:31 +0530

    GraphQL

---
+++ GraphQL/fakeDB.js
@@ -0,0 +1,53 @@
+export const users =[
+    {
+        id:1,
+        Name:"Shakti",
+        email:"shakti@gmail.com",
+    },
+    {
+        id:2,
+        Name:"Radha",
+        email:"radha@gmail.com",
+    },
+    {
+        id:3,
+        Name:"Madhav",
+        email:"madhav@gmail.com",
+    },
+     {
+        id:4,
+        Name:"Hari Om",
+        email:"Hari@gmail.com",
+    }
+]
+
+export const queries=[
+    {
+        id:1,
+        title:"delete user"
+    },
+    {
+        id:2,
+        title:"add new user"
+    },
+    {
+        id:3,
+        title:"Update Username"
+    },
+    {
+        id:4,
+        title:"delete all users"
+    },
+     {
+        id:1,
+        title:"add new user"
+    },
+     {
+        id:4,
+        title:"add new user"
+    },
+     {
+        id:2,
+        title:"Update Username"
+    }
+]
--- GraphQL/index.js
+++ GraphQL/index.js
@@ -1,64 +1,113 @@
+
 // npm i graphql@15.7.2 apollo-server@3.5.0 apollo-server-core@3.5.0
 
-import { ApolloServer,gql } from "apollo-server";
+import { ApolloServer, gql } from "apollo-server";
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import { queries, users } from "./fakeDB.js";
 
-const typeDefs= gql`
-type User{
-name: String,
-email:String
-}
+const typeDefs = gql`
+  type User {
+    name: String
+    email: String
+  }
 
-type Products{
-id:ID,
-productname:String,
-price:String,
-Descripion:String
-}
+  type Products {
+    id: ID
+    productname: String
+    price: String
+    description: String
+  }
 
-type Query{
-    greet:String,
-    user:User
-    product:Products
+  type UsersType {
+    id: ID
+    name: String
+    email: String
+    queries: [QueryType]
+  }
+
+  type QueryType {
+    id: ID
+    title: String
+  }
+
+  type Query {
+    greet: String
+    user: User
+    product: Products
+    users: [UsersType]
+    queries: [QueryType]
+    userByID(id: ID!): UsersType
+  }
+
+ type Mutation {
+
+  addUser(
+    id: ID!,
+    name: String!,
+    email: String!
+  ): UsersType
 }
-`
-const resolvers={
-    Query:{
-        greet: ()=>{ return "Welcome user..."},
-        user: () => {
-        return {
+`;
+
+const resolvers = {
+  Query: {
+    greet: () => {
+      return "Welcome user...";
+    },
+
+    user: () => {
+      return {
         name: "Aparna",
-        email: "aparna@gmail.com"
-        };
-        },
-        product:()=>{
-            return {
-                id:101,
-                productname:"Electronic Watch",
-                price:"699",
-                Descripion:"Stay effortlessly on time with our precision-driven electric timepiece. Designed with a sleek, high-contrast digital screen."
-            }
-        }
-    }
+        email: "aparna@gmail.com",
+      };
+    },
+
+    product: () => {
+      return {
+        id: 101,
+        productname: "Electronic Watch",
+        price: "699",
+        description:
+          "Stay effortlessly on time with our precision-driven electric timepiece.",
+      };
+    },
+
+    users: () => {
+      return users;
+    },
+
+    queries: () => {
+      return queries;
+    },
+
+    userByID: (_, { id }) => {
+      return users.find((u) => u.id == id);
+    },
+  },
+
+  UsersType: {
+    queries: (user) => {
+      return queries.filter((item) => item.id == user.id);
+    },
+  },
+
+ Mutation: {
+
+  addUser: (_, args) => {
+
+    users.push(args);
+
+    return args;
+  }
 }
+};
 
-const server= new ApolloServer({
-    typeDefs,
-    resolvers,
-    plugins:[ApolloServerPluginLandingPageGraphQLPlayground]
-})
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
 
-server.listen().then(({url})=>{
-
-    console.log("server running at "+url)
-})
-
-// on Browser -> localhost:4000
-// On Client side playground
-// # Write your query or mutation here
-
-// # {greet}
-
-// # {user {name}}
-
-// {product {id,productname,price}}
+server.listen().then(({ url }) => {
+  console.log("Server running at " + url);
+});
